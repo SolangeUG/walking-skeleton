6660997be82f34a818d9e767a3065f2bf0b94b6f
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-12 13:56:05 +0000

    Look up port to use from  on startup

--- src/main/java/com/codurance/Main.java
+++ src/main/java/com/codurance/Main.java
@@ -2,7 +2,14 @@ package com.codurance;
 
 public class Main {
 
-  public static void main(String[] args) {
-    new ShoppingBasket().startOn(4567);
-  }
+    public static void main(String[] args) {
+        new ShoppingBasket().startOn(getPortFromEnvironment());
+    }
+
+    private static int getPortFromEnvironment() {
+        String port = System.getenv("PORT");
+        if (port != null)
+            return Integer.parseInt(port);
+        return 4567;
+    }
 }

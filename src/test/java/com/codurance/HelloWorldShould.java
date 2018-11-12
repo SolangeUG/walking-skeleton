151ac49d0d6b2469aaaf7a14ab775c86d4c3d120
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-11-12 09:39:14 +0000

    Add a failing test

---
+++ src/test/java/com/codurance/HelloWorldShould.java
@@ -0,0 +1,13 @@
+package com.codurance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class HelloWorldShould {
+
+    @Test
+    public void failt_this_test() {
+        fail();
+    }
+}
